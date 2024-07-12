0df84a5fe7aa085b77157fb2201d84ff978d1ff6
Mehdi-Ilyes <mehdi-ilyes.haddoune@polymtl.ca>
2024-07-12 12:11:55 -0400

    Small details

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 """
 File: main.py
 Author: BioSensUM 
-Description: This script will use an Emstat3 to perform SWV with predefined parameters. The CSV output will
-             then be treated and analyzed to obtain the gain. The output will then be printed to the user
-             along with the plot obtained.
+Description: Ce script utilisera un Emstat3 pour effectuer un SWV avec des paramètres prédéfinis. Le fichier CSV sera
+             traité et analysé sous forme de pandas.dataframe. Le gain sera alors calculé et affiché à l'utilisateur
+             avec le graphique obtenu.
 """
 import plot_advanced_swv
+import utils
 
 def main():
-    #Faire la SWV(+Reverse SWV)
+    #Faire la SWV(+Reverse SWV) avec comme resultat un output.csv
     plot_advanced_swv.SWV_Console()
-    
+
+    #Recuperer les data et clean-up
+    df = utils.read_voltage_current_from_csv("output.txt")
+    cleaned_df = utils.clean_data(df)
+    smoothed_df = utils.smooth_data(cleaned_df)
 
 if __name__ == '__main__':
     main()
